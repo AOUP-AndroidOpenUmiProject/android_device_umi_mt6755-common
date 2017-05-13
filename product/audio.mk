c624d78870a7bec9b5461972aab6a73b21de95e1
Joe <joe2k01dev@gmail.com>
2017-05-13 15:04:56 +0200

    product/ for common device tree

---
+++ product/audio.mk
@@ -0,0 +1,13 @@
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio_policy.default \
+    libaudio-resampler \
+    libaudiopolicymanagerdefault \
+    libmtk_symbols \
+    libtinycompress \
+    libtinyxml \
+    tinymix \
+    libtinyalsa
