5ff3175aa2112476243afea3b3d54d742bfcc17a
Joe <joe2k01dev@gmail.com>
2017-05-13 14:32:49 +0200

    board/ for common device tree

---
+++ board/device.mk
@@ -0,0 +1,5 @@
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+#Use dlmalloc instead of jemalloc for mallocs
+MALLOC_IMPL := dlmalloc
---
+++ board/telephony.mk
@@ -0,0 +1,3 @@
+# RIL
+BOARD_RIL_CLASS := ../../../device/umi/mt6755-common/ril
+BOARD_CONNECTIVITY_MODULE := conn_soc
